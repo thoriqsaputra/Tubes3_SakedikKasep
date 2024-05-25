e04f91d6f11eee5c059fa385734e7fffb6ef8211
Rizqika Mulia Pratama <rizqikapratamaa@gmail.com>
2024-05-25 10:11:21 +0700

    feat: new database

--- algo/alay_generator.py
+++ algo/alay_generator.py
@@ -1,32 +1,27 @@
 import random
 import re
 
-# Definisikan replacements
 replacements = {
-    'a': ['4', 'A', 'a'], 'b': ['B', 'b', '8', '13'], 'c': ['C', 'c'],
+    'a': ['4', 'A', 'a'], 'b': ['B', 'b', '8'], 'c': ['C', 'c'],
     'd': ['D', 'd'], 'e': ['3', 'E', 'e'], 'f': ['F', 'f'],
-    'g': ['G', 'g', '6', '9'], 'h': ['H', 'h'], 'i': ['1', 'I', 'i', '!'],
-    'j': ['J', 'j'], 'k': ['K', 'k'], 'l': ['L', 'l', '1'],
+    'g': ['G', 'g', '6'], 'h': ['H', 'h'], 'i': ['1', 'I', 'i'],
+    'j': ['J', 'j'], 'k': ['K', 'k'], 'l': ['L', 'l'],
     'm': ['M', 'm'], 'n': ['N', 'n'], 
-    'o': ['0', 'O', 'o'], 'p': ['P', 'p'], 'q': ['Q', 'q', '9'],
-    'r': ['R', 'r', '12', 'i2', 'I2'], 's': ['S', 's', '5'], 't': ['T', 't', '7'],
+    'o': ['0', 'O', 'o'], 'p': ['P', 'p'], 'q': ['Q', 'q'],
+    'r': ['R', 'r'], 's': ['S', 's', '5'], 't': ['T', 't', '7'],
     'u': ['U', 'u'], 'v': ['V', 'v'], 'w': ['W', 'w'],
     'x': ['X', 'x'], 'y': ['Y', 'y'], 'z': ['Z', 'z', '2']
 }
 
-# Inisialisasi reverse_replacements dan regex_pattern
 reverse_replacements = {}
 for key, values in replacements.items():
     for value in values:
         reverse_replacements[value.lower()] = key
-reverse_replacements["12"] = "r"
-reverse_replacements["i2"] = "r"
-reverse_replacements["I2"] = "r"
 
 sorted_keys = sorted(reverse_replacements.keys(), key=len, reverse=True)
 regex_pattern = '|'.join(re.escape(key) for key in sorted_keys)
 
-def alay_converter_advanced(full_name, use_number_symbol=True, use_case_mix=True, use_vowel_removal=False):
+def alay_converter_advanced(full_name, use_number_symbol, use_case_mix, use_vowel_removal):
     modified_name = full_name.lower()
     
     if use_number_symbol:
@@ -38,6 +33,8 @@ def alay_converter_advanced(full_name, use_number_symbol=True, use_case_mix=True
     if use_vowel_removal:
         vowels = 'aeiou'
         modified_name = ''.join(char for char in modified_name if char not in vowels and char.lower() not in vowels)
+
+    
     
     return modified_name
 
@@ -51,8 +48,7 @@ def revert_alay_name(alay_name):
     
     return modified_name
 
-# Input dari pengguna
-full_name = input("Masukkan nama lengkap: ")
-random_alay_version = alay_converter_advanced(full_name, use_number_symbol=True, use_case_mix=True, use_vowel_removal=False)
-print("Nama alay: " + random_alay_version)
-print("Nama Revert: " + revert_alay_name(random_alay_version))
+# full_name = input("Masukkan nama lengkap: ")
+# random_alay_version = alay_converter_advanced(full_name, use_number_symbol=random.choice([True, False]), use_case_mix=random.choice([True, False]), use_vowel_removal=random.choice([True, False]))
+# print("Nama alay: " + random_alay_version)
+# print("Nama Revert: " + revert_alay_name(random_alay_version))
--- algo/db_scraper.py
+++ algo/db_scraper.py
@@ -13,27 +13,35 @@ c = conn.cursor()
 # Membuat tabel jika belum ada
 c.execute('''
 CREATE TABLE IF NOT EXISTS biodata (
-    NIK TEXT, 
-    nama TEXT, 
-    tempat_lahir TEXT, 
-    tanggal_lahir TEXT, 
+    NIK VARCHAR(16) PRIMARY KEY NOT NULL, 
+    nama VARCHAR(100) DEFAULT NULL, 
+    tempat_lahir VARCHAR(50) DEFAULT NULL, 
+    tanggal_lahir DATE DEFAULT NULL, 
     jenis_kelamin TEXT, 
-    golongan_darah TEXT, 
-    alamat TEXT, 
-    agama TEXT, 
+    golongan_darah VARCHAR(5) DEFAULT NULL, 
+    alamat VARCHAR(255) DEFAULT NULL, 
+    agama VARCHAR(50) DEFAULT NULL, 
     status_perkawinan TEXT, 
-    pekerjaan TEXT, 
-    kewarganegaraan TEXT
+    pekerjaan VARCHAR(100) DEFAULT NULL, 
+    kewarganegaraan VARCHAR(50) DEFAULT NULL
+)
+''')
+
+c.execute('''
+CREATE TABLE IF NOT EXISTS sidik_jari (
+    berkas_citra TEXT,
+    nama VARCHAR(100) DEFAULT NULL
 )
 ''')
 
 # Generate records
-for _ in range(num_records):
+for i in range(num_records):
+    data_nama = fake.name()
     c.execute('''
     INSERT INTO biodata (NIK, nama, tempat_lahir, tanggal_lahir, jenis_kelamin, golongan_darah, alamat, agama, status_perkawinan, pekerjaan, kewarganegaraan) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
     ''', (
         str(fake.unique.random_number(digits=16, fix_len=True)),
-        alay_converter_advanced(fake.name()),
+        alay_converter_advanced(data_nama, use_number_symbol=random.choice([True, False]), use_case_mix=random.choice([True, False]), use_vowel_removal=random.choice([True, False])),
         fake.city(),
         fake.date_of_birth(minimum_age=18, maximum_age=90).strftime('%Y-%m-%d'),
         random.choice(['Laki-Laki', 'Perempuan']),
@@ -43,6 +51,12 @@ for _ in range(num_records):
         random.choice(['Belum Menikah', 'Menikah', 'Cerai']),
         fake.job(),
         'Indonesia'
+    ))
+    c.execute('''
+    INSERT INTO sidik_jari (berkas_citra, nama) VALUES (?, ?)
+    ''', (
+        str(i),
+        data_nama
     ))
 
 conn.commit()
